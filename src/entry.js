b1828af99d7a122d1343e60c786ad105704702bb
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-02-01 22:11:07 -0300

    Initial commit

---
+++ src/entry.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class Lelao extends PureComponent {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
