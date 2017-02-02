e0c92917fee74d4950a36f66eabf1989b6f49378
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-02-01 23:05:42 -0300

    Defines the expected plugins prop

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,16 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, PropTypes } from 'react'
 
 export default class Lelao extends PureComponent {
+  static propTypes = {
+    plugins: PropTypes.objectOf(
+      PropTypes.shape({
+        Handler: PropTypes.func.isRequired,
+        Input: PropTypes.func.isRequired,
+        Preview: PropTypes.func.isRequired
+      })
+    )
+  }
+
   render() {
     return (
       <div>
